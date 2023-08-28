322845451b627dc7ce9f412f44bdb1655181cf97
saber <sbaerm.com@gmail.com>
2023-08-28 19:30:07 +0330

    add application context

--- src/main/java/util/ApplicationContext.java
+++ src/main/java/util/ApplicationContext.java
@@ -1,4 +1,27 @@
 package util;
 
+import dto.DtoMapper;
+import dto.impl.DtoMapperImpl;
+import jakarta.persistence.EntityManager;
+import repository.UserRepository;
+import repository.impl.UserRepositoryImpl;
+import service.UserService;
+import service.impl.UserServiceImpl;
+
 public class ApplicationContext {
+    static EntityManager entityManager = HibernateUtils.getEntityManagerFactory().createEntityManager();
+
+    //------------ user application context ------------------//
+
+    private static final UserService userService;
+
+    static {
+        DtoMapper dtoMapper = new DtoMapperImpl();
+        UserRepository userRepository = new UserRepositoryImpl(entityManager);
+        userService = new UserServiceImpl(userRepository, dtoMapper);
+    }
+
+    public static UserService getUserService() {
+        return userService;
+    }
 }
